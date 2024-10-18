7b2903c12223621e2f5a696479bdc7b65bf61e7d
chiemezie1 <chiemezieagbo1@gmail.com>
2024-10-18 03:44:56 +0100

    prime hub

--- src/app/api/admin/products/users/route.ts
+++ src/app/api/admin/products/users/route.ts
@@ -13,7 +13,7 @@ export async function GET() {
     return NextResponse.json(users);
   } catch (error) {
     console.error('Error fetching users:', error);
-    return NextResponse.json({ error: 'Failed to fetch users' }, { sstatus: 500 });
+    return NextResponse.json({ error: 'Failed to fetch users' }, { status: 500 });
   }
 }
 
